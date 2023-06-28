8dcf5adfc1b8c4f550902cdbd7cbc07c34b4569b
Ryszard Schossler <ryszard.schossler@gmail.com>
2023-06-28 20:47:27 +0200

    init image cubit

---
+++ lib/cubit/image_state.dart
@@ -0,0 +1,10 @@
+part of 'image_cubit.dart';
+
+abstract class ImageState extends Equatable {
+  const ImageState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ImageInitial extends ImageState {}
